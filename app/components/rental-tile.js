6d81781c6f5cbe1bb8ef7e1c9a50ed42b2e81fb9
Matt Wilkin <mwilkin22@gmail.com>
2016-08-18 11:15:03 -0700

    add dynanic routing for rental listings.

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
-  updateRentalForm: false,
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
