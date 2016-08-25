021955325bc32d637afcfd4a095f8cfe3de60084
Matt Wilkin <mwilkin22@gmail.com>
2016-08-25 16:21:27 -0700

    refactor adding rentals from cities.

--- app/models/city.js
+++ app/models/city.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   country: DS.attr(),
-  rentals: DS.hasMany('rentals', {async: true}),
+  rentals: DS.hasMany('rentals', {async: true})
 });
